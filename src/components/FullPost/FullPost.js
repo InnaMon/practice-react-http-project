de1cc1cfe9feb3a6d7db04f35013976dd7143990
Inna Mon <inna.mon@gmail.com>
2019-08-20 19:24:11 -0500

    added state and a componentDidUpdate lifecycle hook to FullPost comp in order to fetch post data and post it using a post's unique id

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -3,13 +3,36 @@ import React, { Component } from 'react';
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate() {
+        if (this.props.id) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                fetch('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => response.json())
+                .then(post => 
+                    // console.log('postid:', post)
+                    this.setState({loadedPost: post}));
+            }
+        }
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        // check that a props.id exists and display loading since will recieve 
+        // props before het new loadedPost state due to asynch behavior of fetching data in componentDidUpdate
         if (this.props.id) {
+            <p style={{textAlign: 'center'}}>Loading...!</p>
+        }
+
+        //only execute below code if loadedPost state has been set to truw with updated fetch data 
+        if (this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
